3dcb1d34849722b0152459dc2358af1e94385340
Chris Mahr <mahrtian@gmail.com>
2016-07-11 16:06:28 -0400

    completed section 2 of shaping up with angular

--- app.js
+++ app.js
@@ -5,18 +5,50 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    }
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: '...',
       canPurchase: true,
+      images: [
+        {
+          full: 'dodecahedron-01-full.jpg',
+          thumb: 'dodecahedron-01-thumb.jpg'
+        },
+        {
+          full: 'dodecahedron-02-full.jpg',
+          thumb: 'dodecahedron-02-thumb.jpg'
+        }
+      ]
     },
     {
       name: 'Pentagonal Gem',
       price: 5.95,
       description: '...',
-      canPurchase: false
+      canPurchase: false,
+      images: [
+        {
+          full: 'dodecahedron-01-full.jpg',
+          thumb: 'dodecahedron-01-thumb.jpg'
+        },
+        {
+          full: 'dodecahedron-02-full.jpg',
+          thumb: 'dodecahedron-02-thumb.jpg'
+        }
+      ]
     }
   ];	
 })();
