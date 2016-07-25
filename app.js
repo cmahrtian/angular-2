135efb675fe594d30eea0a7bff25f2ac57ecb741
Chris Mahr <mahrtian@gmail.com>
2016-07-25 09:28:42 -0400

    completed angular tutorial through lesson 3

--- app.js
+++ app.js
@@ -17,6 +17,15 @@
     }
   });
 
+  app.controller("ReviewController", function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
@@ -31,6 +40,18 @@
         {
           full: 'dodecahedron-02-full.jpg',
           thumb: 'dodecahedron-02-thumb.jpg'
+        }
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: "I love this product!",
+          author: "joe@thomas.com"
+        },
+        {
+          stars: 1,
+          body: "This product sucks",
+          author: "tim@hater.com"
         }
       ]
     },
