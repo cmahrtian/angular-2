7cdcc8dca958dba5d83476227a5c489d3f7bc8cc
Chris Mahr <mahrtian@gmail.com>
2016-07-25 21:25:27 -0400

    completed lesson 4 of angular tutorial

--- app.js
+++ app.js
@@ -7,14 +7,12 @@
 
   app.controller('PanelController', function() {
     this.tab = 1;
-
     this.selectTab = function(setTab) {
       this.tab = setTab;
     };
-
     this.isSelected = function(checkTab) {
       return this.tab === checkTab;
-    }
+    };
   });
 
   app.controller("ReviewController", function() {
@@ -23,6 +21,31 @@
     this.addReview = function(product) {
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+  app.directive('productTitle', function() {
+    return {
+      // A configuration object defining how your directive will work
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    };
+  });
+
+  app.directive('productPanels', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller:function() {
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'tab'
     };
   });
 
